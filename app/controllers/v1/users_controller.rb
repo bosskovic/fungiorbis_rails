2af8eb71614670b33a33d1055f49ef423239d055
Marko Bošković <bosskovic@gmail.com>
2014-08-07 22:55:27 +0200

    adds users route and controller

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,12 @@
+class V1::UsersController < ApplicationController
+
+  respond_to :json
+
+  def index
+    users =  User.all
+    render json: users, status: :ok
+  end
+
+  def create
+  end
+end
