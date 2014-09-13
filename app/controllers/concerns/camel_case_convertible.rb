0e9fffae47fa5e5b8ad2c43d4e8f52cf35e75474
Marko Bošković <bosskovic@gmail.com>
2014-09-13 15:42:08 +0200

    extracts camel case conversion to lib

--- app/controllers/concerns/camel_case_convertible.rb
+++ app/controllers/concerns/camel_case_convertible.rb
@@ -1,31 +1,6 @@
+require 'camel_case'
 module CamelCaseConvertible
   extend ActiveSupport::Concern
 
-  def keys_to_underscore(params)
-    r = {}
-    params.each_key do |key|
-      r[key.underscore] = params[key]
-    end
-    r
-  end
-
-  def keys_to_camel_case(params)
-    r = {}
-    params.each_key do |key|
-      r[key.to_s.camelize(:lower)] = params[key]
-    end
-    r
-  end
-
-
-
-  private
-
-  def underscore
-    self.gsub(/::/, '/').
-        gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
-        gsub(/([a-z\d])([A-Z])/, '\1_\2').
-        tr("-", "_").
-        downcase
-  end
+  include CamelCase
 end
---
+++ spec/lib/camel_case_spec.rb
@@ -0,0 +1,101 @@
+require 'camel_case'
+
+RSpec.describe CamelCase do
+
+  include CamelCase
+
+  def hash
+    { context: 'hash',
+      camel_sym: { myFirstName: 'John', lastName: 'Smith' },
+      snake_sym: { my_first_name: 'John', last_name: 'Smith' },
+      camel_str: { 'myFirstName' => 'John', 'lastName' => 'Smith' },
+      snake_str: { 'my_first_name' => 'John', 'last_name' => 'Smith' } }
+  end
+
+  def array
+    { context: 'array',
+      camel_sym: [:myFirstName, :lastName],
+      snake_sym: [:my_first_name, :last_name],
+      camel_str: %w(myFirstName lastName),
+      snake_str: %w(my_first_name last_name) }
+  end
+
+  def string
+    { context: 'string',
+      camel_sym: :myFirstName,
+      snake_sym: :my_first_name,
+      camel_str: 'myFirstName',
+      snake_str: 'my_first_name' }
+  end
+
+
+  describe '#to_underscore' do
+    [:hash, :array, :string].each do |e|
+      context "when argument is a #{e}" do
+        before (:each) do
+          @obj = send e
+        end
+
+        context 'when term is symbol' do
+          it 'outputs the symbols if the option is not set otherwise' do
+            expect(to_underscore(@obj[:camel_sym])).to eq @obj[:snake_sym]
+          end
+          it 'outputs the strings because the option is set so' do
+            expect(to_underscore(@obj[:camel_sym], output: 'strings')).to eq @obj[:snake_str]
+          end
+        end
+
+        context 'when term is string' do
+          it 'outputs the strings if the option is not set otherwise' do
+            expect(to_underscore(@obj[:camel_str])).to eq @obj[:snake_str]
+          end
+          it 'outputs the symbols because the option is set so' do
+            expect(to_underscore(@obj[:camel_str], output: 'symbols')).to eq @obj[:snake_sym]
+          end
+        end
+
+        context 'when argument is already in snake case' do
+          it 'does not affect snake case' do
+            expect(to_underscore(@obj[:snake_sym])).to eq @obj[:snake_sym]
+            expect(to_underscore(@obj[:snake_str])).to eq @obj[:snake_str]
+          end
+        end
+      end
+    end
+  end
+
+  describe '#to_camel_case' do
+    [:hash, :array, :string].each do |e|
+      context "when argument is a #{e}" do
+        before (:each) do
+          @obj = send e
+        end
+
+        context 'when term is symbol' do
+          it 'outputs the symbols if the option is not set otherwise' do
+            expect(to_camel_case(@obj[:snake_sym])).to eq @obj[:camel_sym]
+          end
+          it 'outputs the strings because the option is set so' do
+            expect(to_camel_case(@obj[:snake_sym], output: 'strings')).to eq @obj[:camel_str]
+          end
+        end
+
+        context 'when term is string' do
+          it 'outputs the strings if the option is not set otherwise' do
+            expect(to_camel_case(@obj[:snake_str])).to eq @obj[:camel_str]
+          end
+          it 'outputs the symbols because the option is set so' do
+            expect(to_camel_case(@obj[:snake_str], output: 'symbols')).to eq @obj[:camel_sym]
+          end
+        end
+
+        context 'when argument is already in snake case' do
+          it 'does not affect snake case' do
+            expect(to_camel_case(@obj[:camel_sym])).to eq @obj[:camel_sym]
+            expect(to_camel_case(@obj[:camel_str])).to eq @obj[:camel_str]
+          end
+        end
+      end
+    end
+  end
+end
