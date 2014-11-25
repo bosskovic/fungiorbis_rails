726f4d4b0ff35c9e765a7117c9e13a2f67b765b5
Marko Bošković <bosskovic@gmail.com>
2014-11-25 03:35:48 +0100

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :log_level, :debug
 
 set :pty, true
 
-set :linked_files, %w{config/database_example.yml config/secrets_example.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :keep_releases, 3
