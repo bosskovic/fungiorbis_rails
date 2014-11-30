b187e93682a9c3dd1dfd35f02220452c874f302f
Marko Bošković <bosskovic@gmail.com>
2014-11-30 23:45:09 +0100

    fix

--- app/controllers/v1/habitats_controller.rb
+++ app/controllers/v1/habitats_controller.rb
@@ -7,7 +7,7 @@ class V1::HabitatsController < ApplicationController
   def show
     authorize! :show, :habitats
     result = {}
-    Array(['en']).each do |locale|
+    Array(['sr']).each do |locale|
       result[locale] = habitats_yaml(locale)
     end
     render json: result
--- app/controllers/v1/substrates_controller.rb
+++ app/controllers/v1/substrates_controller.rb
@@ -7,7 +7,7 @@ class V1::SubstratesController < ApplicationController
   def show
     authorize! :show, :substrates
     result = {}
-    Array(['en']).each do |locale|
+    Array(['sr']).each do |locale|
       result[locale] = substrates_yaml(locale)
     end
     render json: result
